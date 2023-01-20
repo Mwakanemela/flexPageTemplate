2c853ee8a73a9ed4105cdbe4b1291778716f56ea
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-01-20 16:24:05 +0300

    Finalized the template

--- app.js
+++ app.js
@@ -12,5 +12,36 @@ linkz.forEach((el)=> {
         }
 
         el.classList.add('active');
+        console.log(el.hash);
+        let stopX = document.querySelector(el.hash).offsetTop;
+        let startX = document.documentElement.scrollTop;
+        console.log(stopX);
+        console.log(startX);
+
+        myMove(startX, stopX);
     })
 })
+
+function myMove(startX, stopX)
+{
+    const speed = 10;
+    const pos = stopX > startX ? 1 : -1;
+    let move = Math.floor((stopX - startX) * pos / speed);
+    const adder = pos * speed;
+    console.log(move, adder);  
+
+    let framez = setInterval(frame, 1);
+    function frame() {
+        if(move < 0) {
+            clearInterval(framez);
+        }
+
+        else 
+        {
+            move--;
+            startX = startX + adder;
+            document.documentElement.scrollTop = startX;
+            console.log(startX);
+        }
+    }
+}
